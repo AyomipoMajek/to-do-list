e0383b66ff40f5202915d28be33a8d376a935f5b
Ayomipo Majek <ayomipomajek@gmail.com>
2023-01-21 21:33:22 +0100

    linters fixed

--- src/module/checker.js
+++ src/module/checker.js
@@ -1,46 +1,42 @@
-import updateLocalStorage from "./localstorage";
+import updateLocalStorage from './localstorage';
 
-const checker = (te,bo,to) => {
-//check
-  bo.forEach((e,n)=>{
-    e.addEventListener('click',()=>{
-        let parent = e.parentElement;
-        parent.classList.add('check');
-        let grand = e.parentElement.parentElement;
-        let line = grand.querySelector('#tasks');
-        line.classList.add('line')
-        grand.setAttribute('id','completed');
+const checker = (te, bo, to) => {
+// check
+  bo.forEach((e, n) => {
+    e.addEventListener('click', () => {
+      const parent = e.parentElement;
+      parent.classList.add('check');
+      const grand = e.parentElement.parentElement;
+      const line = grand.querySelector('#tasks');
+      line.classList.add('line');
+      grand.setAttribute('id', 'completed');
 
-        te.forEach((t,b)=>{
-            if(b === n){
-             t.completed = true;
-            }
-            updateLocalStorage(te)
-        })
-      
-    })
-    
-  })
+      te.forEach((t, b) => {
+        if (b === n) {
+          t.completed = true;
+        }
+        updateLocalStorage(te);
+      });
+    });
+  });
 
-  //uncheck
-  to.forEach((e,n)=>{
-    e.addEventListener('click',()=>{
-        let parent = e.parentElement;
-        parent.classList.remove('check');
-        let grand = e.parentElement.parentElement;
-        let line = grand.querySelector('#tasks');
-        line.classList.remove('line')
-        grand.removeAttribute('id');
-        te.forEach((t,b)=>{
-            if(b === n){
-             t.completed = false;
-            }
-            updateLocalStorage(te)
-        })
-    })
-   
-  })
-
-}
+  // uncheck
+  to.forEach((e, n) => {
+    e.addEventListener('click', () => {
+      const parent = e.parentElement;
+      parent.classList.remove('check');
+      const grand = e.parentElement.parentElement;
+      const line = grand.querySelector('#tasks');
+      line.classList.remove('line');
+      grand.removeAttribute('id');
+      te.forEach((t, b) => {
+        if (b === n) {
+          t.completed = false;
+        }
+        updateLocalStorage(te);
+      });
+    });
+  });
+};
 
 export default checker;
